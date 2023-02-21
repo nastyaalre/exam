6f4bd86c9144e200c673fed881ae3d7f36de89fb
nastyaalre <53492217+nastyaalre@users.noreply.github.com>
2023-02-21 18:59:36 +0400

    Update fizzbuzz.c

--- lvl0/fizzbuzz.c
+++ lvl0/fizzbuzz.c
@@ -1,57 +1,58 @@
-*/ Assignment name  : fizzbuzz
-Expected files   : fizzbuzz.c
-Allowed functions: write
---------------------------------------------------------------------------------
+// */ Assignment name  : fizzbuzz
+// Expected files   : fizzbuzz.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
 
-Write a program that prints the numbers from 1 to 100, each separated by a
-newline.
+// Write a program that prints the numbers from 1 to 100, each separated by a
+// newline.
 
-If the number is a multiple of 3, it prints 'fizz' instead.
+// If the number is a multiple of 3, it prints 'fizz' instead.
 
-If the number is a multiple of 5, it prints 'buzz' instead.
+// If the number is a multiple of 5, it prints 'buzz' instead.
 
-If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+// If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
 
-Example:
+// Example:
 
-$>./fizzbuzz
-1
-2
-fizz
-4
-buzz
-fizz
-7
-8
-fizz
-buzz
-11
-fizz
-13
-14
-fizzbuzz
-[...]
-97
-98
-fizz
-buzz
-$>
-/*
+// $>./fizzbuzz
+// 1
+// 2
+// fizz
+// 4
+// buzz
+// fizz
+// 7
+// 8
+// fizz
+// buzz
+// 11
+// fizz
+// 13
+// 14
+// fizzbuzz
+// [...]
+// 97
+// 98
+// fizz
+// buzz
+// $>
+// /*
+
+#include <unistd.h>
 
 void  ft_putnbr(int i)
 {
-  str nbr[] = "0123456789"
-  while (i > 10)
-    ft_putnbr(i);
-  if (i < 10)
-    write (1, &nbr[i], 1);
+  char nbr[] = "0123456789";
+  if (i > 10)
+    ft_putnbr(i / 10);
+  write (1, &nbr[i%10], 1);
 }
 
 int main()
 {
 int i;
 
-i = 0;
+i = 1;
   while (i <= 100)
   {
     if (i % 3 == 0 && i % 5 == 0)
@@ -60,7 +61,7 @@ i = 0;
       write (1, "fizz", 4);
     else if (i % 5 == 0)
       write (1, "buzz", 4);
-    else 
+    else if (i % 3 != 0 && i % 5 != 0)
       ft_putnbr(i);
      write (1, "\n", 1); 
     i++; 
